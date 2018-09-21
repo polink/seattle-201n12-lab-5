6d0880c27035f35f03d46f8c9bbd7e4d07f13828
Karl Polintan <kf.polintan@gmail.com>
2018-09-20 20:24:28 -0700

    completed sumAndMultiply to work

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-  var sumAdd = [a+b,`The sum of ${a} and ${b} is ${a+b}.`];
-  return sumAdd;
+//  var plus = [a+b,`The sum of ${a} and ${b} is ${a+b}.`];
+//  return plus;
+  return a+b;
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -28,12 +29,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-  var multi = [a*b, `The product of ${a} and ${b} is ${a*b}.`];
-  return multi;
+//  var multi = [a*b, `The product of ${a} and ${b} is ${a*b}.`];
+//  return multi;
+  return a*b;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -50,11 +52,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var multiPlus = [sum(sum(a,b),c),multiply(multiply(a,b),c),`${a} and ${b} and ${c} sum to ${sum(sum(a,b),c)}.`,`The product of ${a} and ${b} and ${c} is ${multiply(multiply(a,b),c)}.`];
+  return multiPlus; //only not supposed to use plus and asterisk in THIS function, correct?
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
